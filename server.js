43f54825db061b0ce15590cc1abff5b607dc590a
Jesús Mercado <jesusmmm@gmail.com>
2013-12-10 00:26:22 -0200

    Adicionar cabezales HTTP y especificar que la respuesta está en formato JSON

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var http = require('http');
 var PORT = process.env.PORT || 5000;
 
 http.createServer(function(req, res) {
+    res.setHeader('Content-Type', 'application/json');
     res.end(JSON.stringify({alive: true}));
 }).listen(PORT);
 
